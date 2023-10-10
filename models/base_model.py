00213045644e523f15e93875f623dc3fe4e03a00
Mahmoud Salah <ms685116@gmail.com>
2023-10-10 14:38:25 +0300

    update init method

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,20 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialization method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ("updated_at", "created_at"):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """str method of the class"""
