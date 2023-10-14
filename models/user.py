fe60e67249cd37329917e9402ab172beb1b8bb3a
dawoud <mhmaddawoud20@gmail.com>
2023-10-14 15:22:33 +0300

    Update some files

--- models/user.py
+++ models/user.py
@@ -11,7 +11,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """Initialization"""
-        super().__init__(*args, **kwargs)
